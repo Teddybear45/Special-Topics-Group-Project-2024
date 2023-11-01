7b12205b709aeb9f179e6b8f3f9940de4a66d5c8
Nathan <nathansun1234@gmail.com>
2023-11-01 16:00:24 -0700

    demo 1

---
+++ convert.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import cv2
+
+#converts a binary mask to the yolov5 label format
+def mask_to_label(mask_path: str, label_path: str, class_id: int):
+    # load the mask image and convert it to grayscale
+    mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+
+    # find contours in the mask image
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour = max(contours, key=cv2.contourArea)
+    with open(label_path, 'w') as label_file:
+        # convert contour to polygon format
+        polygon_points = contour.reshape(-1, 2)
+        normalized_polygon = [(x / mask.shape[1], y / mask.shape[0]) for x, y in polygon_points]
+    
+        # writes the coordinates to label txt file
+        label_file.write(f"{class_id} ")
+        for x, y in normalized_polygon:
+            label_file.write(f"{x} {y} ")
+
+#converts a folder of binary masks to yolov5 label format
+def mask_to_label_dir(input_dir: str, output_dir: str):
+    counter = 0
+    os.makedirs(output_dir, exist_ok=True)
+
+    for mask_filename in os.listdir(input_dir):
+        if mask_filename.endswith('.jpg'):
+            mask_image_path = os.path.join(input_dir, mask_filename)
+            label_txt_path = os.path.join(output_dir, mask_filename.replace('.jpg', '.txt'))
+            mask_to_label(mask_image_path, label_txt_path, 0)
+            print("converted " + mask_filename)
+            counter += 1
+            
+    print("")
+    print("converted " + str(counter) + " masks in " + input_dir + " to labels in " + output_dir)
+
+
+#converts the yolov5 label to a list of polygon coords
+def get_polygon_coords(label_path: str):
+    # load the yolov5 label txt file
+    with open(label_path, 'r') as label_file:
+        lines = label_file.readlines()
+
+    # parse the polygon coordinates from the label file
+    polygon_coords = []
+    for line in lines:
+        parts = line.strip().split()
+        class_idx = int(parts[0])
+        coords = [(float(parts[i]), float(parts[i + 1])) for i in range(1, len(parts), 2)]
+        polygon_coords.append((class_idx, coords))
+    
+    return polygon_coords
+
+#creates a binary mask as a numpy array from a list of polygon coords
+def create_mask_image(polygon_coords: list, mask_size: tuple):
+    # creates empty mask array
+    mask = np.zeros(mask_size, dtype=np.uint8)
+
+    for class_idx, coords in polygon_coords:
+        pts = np.array(coords)
+        unnormalized_pts = (pts * np.array(mask_size[::-1])).astype(int)  # reverse mask_size for (height, width)
+        cv2.fillPoly(mask, [unnormalized_pts], 255)
+    
+    return mask
+
+def label_to_mask_dir(input_dir: str, output_dir: str, mask_size: tuple):
+    os.makedirs(output_dir, exist_ok=True)
+    counter = 0
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith('.txt'):
+            txt_path = os.path.join(input_dir, filename)
+            mask_path = os.path.join(output_dir, filename.replace('.txt', '.jpg'))
+
+            polygon_points = get_polygon_coords(txt_path)
+            mask = create_mask_image(polygon_points, mask_size)
+            
+            cv2.imwrite(mask_path, mask)
+            counter += 1
+            print("converted " + filename)
+
+    print("")
+    print("converted " + str(counter) + " labels in " + input_dir + " to masks in " + output_dir)
+
